190dbd69d19b7e80d3f154eeec8ca210b5ac572b
Chris DeMars <cdemars86@gmail.com>
2020-01-30 16:58:57 -0700

    things

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const paragraphs = document.getElementById('paragraphs');
+const demars = document.getElementById('demarsSteez');
+const lorem = document.getElementById('lorem');
+const generate = document.getElementById('generate');
+const generatedIpsum = document.getElementById('lorem-paragraphs');
